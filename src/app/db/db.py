19fffb5160b8bdd49eef585d0b1f215dfad96c16
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-30 15:20:53 -0400

    Refactor database connection to use environment variable for DATABASE_URL

--- src/app/db/db.py
+++ src/app/db/db.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DB_USER = "realtime_user"
-DB_PASS = "realtime_pass"
-DB_HOST = "localhost"
-DB_PORT = "5433"
-DB_NAME = "realtime_db"
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
